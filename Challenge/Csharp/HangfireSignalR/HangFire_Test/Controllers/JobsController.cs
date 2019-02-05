5ef3969b4bb0d461c6da657cf9092449e653f87b
Polanco <jdanypa@gmail.com>
2019-02-04 21:48:01 -0600

    10th day working

---
+++ Challenge/Csharp/HangfireSignalR/HangFire_Test/Controllers/JobsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Hangfire;
+using HangFire_Test.Hubs;
+using HangFire_Test.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace HangFire_Test.Controllers
+{
+    public class ReciberData
+    {
+        public int Time { get; set; }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly IHubContext<NotificationsHub> hubContext;
+
+        public JobsController(IHubContext<NotificationsHub> hubContext)
+        {
+            this.hubContext = hubContext;
+        } 
+        [HttpPost("[action]")]
+        public IActionResult AddJob([FromBody] ReciberData data)
+        {
+            BackgroundJob.Enqueue(() => this.Work(data.Time));
+            return Ok();
+        }
+
+        public void Work(int timer)
+        {
+            // Sleep time simulation of the timer
+            Thread.Sleep(timer * 1000);
+            // Sends the notification to the hub
+            this.hubContext.Clients.All.SendAsync("Notifications", new EventNotification() {
+                Title = "Finished Job",
+                Message = "Finished after " + timer.ToString() + " seconds"
+            });
+        }
+    }
+}
---
+++ Challenge/Csharp/HangfireSignalR/HangFire_Test/Hubs/NotificationsHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire_Test.Hubs
+{
+    public class NotificationsHub : Hub
+    {
+        public void SendNotification(string message)
+        {
+            Clients.All.SendAsync("Notifications", message);
+        }
+    }
+}
